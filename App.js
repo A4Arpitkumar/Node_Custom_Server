f88266bf331b0aae76403ff66f499eee5ea2a26e
A4Arpitkumar <arpitofficial6@gmail.com>
2022-05-26 05:01:57 -0700

    first_node

---
+++ App.js
@@ -0,0 +1,14 @@
+// var http = require("http");
+
+// http.createServer(function(req,res){
+//     res.writeHead(200 , {'content-type' : 'text/plain'});
+//     res.end("hello How Are You?");
+// }).listen(8000);
+
+
+var  a = 10;
+var  b = 110;
+
+var c = a + b;
+
+console.log(c);
